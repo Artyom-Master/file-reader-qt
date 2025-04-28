740c7fbe2caf65ec0d747bb2f3197ca35d837631
Artyom-Master <arthil155@gmail.com>
2025-04-28 18:46:53 +0300

    add qt tests for cpp classes

---
+++ tests/tst_wordstatsmodel/tst_wordstatsmodel.cpp
@@ -0,0 +1,36 @@
+#include <QCoreApplication>
+#include <QtTest>
+#include "wordstatsmodel.h"
+
+// add necessary includes here
+
+class tst_wordstatsmodel : public QObject
+{
+    Q_OBJECT
+
+public:
+    tst_wordstatsmodel();
+    ~tst_wordstatsmodel();
+
+private:
+    WordStatsModel m_wordStatsModel;
+
+private slots:
+    void initTestCase();
+    void cleanupTestCase();
+    void test_case1();
+};
+
+tst_wordstatsmodel::tst_wordstatsmodel() {}
+
+tst_wordstatsmodel::~tst_wordstatsmodel() {}
+
+void tst_wordstatsmodel::initTestCase() {}
+
+void tst_wordstatsmodel::cleanupTestCase() {}
+
+void tst_wordstatsmodel::test_case1() {}
+
+QTEST_MAIN(tst_wordstatsmodel)
+
+#include "tst_wordstatsmodel.moc"
